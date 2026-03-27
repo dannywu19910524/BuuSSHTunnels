5569438d4f82242f3c21bb58525d578c6a064af9
dannywu19910524 <dannywu19910524@gmail.com>
2026-03-27 23:05:52 +0800

    feat: add NotificationService and LoginItemManager

---
+++ Sources/Tunnel/Services/NotificationService.swift
@@ -0,0 +1,33 @@
+import Foundation
+import UserNotifications
+
+class NotificationService: NSObject, UNUserNotificationCenterDelegate {
+    override init() {
+        super.init()
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound]) { _, _ in }
+    }
+
+    func send(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: nil
+        )
+        UNUserNotificationCenter.current().add(request)
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .sound])
+    }
+}
